134be32875077a17b18719f6b09168d093937779
ASSASSI <ya2257@att.com>
2017-09-29 23:34:35 -0400

    Add all db methods

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://younessassassi:echofabulous1@ds157624.mlab.com:57624/heroku_tljqf0tk');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+bookRouter = require('./routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('welcome to my new API');
@@ -11,3 +24,5 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('My app is running on PORT: ' + port);
 });
+
+// MONGODB_URI
---
+++ routes/bookRoutes.js
@@ -0,0 +1,103 @@
+var express = require('express'),
+    booksJson = require('../data/bookJson.js');
+
+var routes = function (Book) {
+    var bookRouter = express.Router();
+
+    bookRouter.route('/')
+        .post(function(req, res) {
+            var book = new Book(req.body);
+
+            book.save(function(err, book) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(201).json(book);
+                }
+            });
+           
+        })
+        .get(function(req, res) {
+            var query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function(err, books) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(books);
+                }
+            });
+        });
+    
+    bookRouter.use('/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else if(book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
+    bookRouter.route('/:bookId')
+        .get(function(req, res) {
+            res.json(req.book);
+        })
+        .put(function(req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function(req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete(function(req, res) {
+            req.book.remove(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('removed');
+                }
+            });
+        });
+
+    bookRouter.route('/admin')
+        .get(function(req, res) {
+            Book.insertMany(booksJson, function(err, data) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(data);
+                }
+            });
+        });
+
+    return bookRouter;
+};
+
+module.exports = routes;
+
